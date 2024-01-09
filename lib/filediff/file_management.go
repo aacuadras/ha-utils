1f6e24949cd1d7fef1106f9593a1033cb49599ec
aacuadras <aaron-98@live.com.mx>
2024-01-09 16:00:57 -0600

    Added method to send file for replacement

--- lib/filediff/file_management.go
+++ lib/filediff/file_management.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func DecodeFile(fileContents string) ([]byte, error) {
+func decodeFile(fileContents string) ([]byte, error) {
 	decodedContents, err := b64.StdEncoding.DecodeString(fileContents)
 	if err != nil {
 		return []byte{}, err
@@ -15,11 +15,29 @@ func DecodeFile(fileContents string) ([]byte, error) {
 	return decodedContents, nil
 }
 
-func IsSameFile(fileName string, atpContent []byte) (bool, error) {
+func IsSameFile(fileName string, encodedContent string) (bool, error) {
+	decodedContent, err := decodeFile(encodedContent)
+	if err != nil {
+		return true, err
+	}
+
 	fileContents, err := os.ReadFile(fileName)
 	if err != nil {
 		return true, err
 	}
 
-	return bytes.Equal(fileContents, atpContent), nil
+	return bytes.Equal(fileContents, decodedContent), nil
+}
+
+func ReplaceFile(fileName string, encodedFileContents string) error {
+	fileContents, err := decodeFile(encodedFileContents)
+	if err != nil {
+		return err
+	}
+
+	if err := os.WriteFile(fileName, fileContents, 0600); err != nil {
+		return err
+	}
+
+	return nil
 }
--- server/files.go
+++ server/files.go
@@ -15,13 +15,34 @@ func NewFileServer() pb.FileUtilsServer {
 	return &fileServer{}
 }
 
-func (s *fileServer) CompareFile(ctx context.Context, in *pb.File) (*pb.FileDiff, error) {
-	decodedContents, err := filediff.DecodeFile(in.EncodedContent)
+func (s *fileServer) SendFile(ctx context.Context, in *pb.File) (*pb.ProcessedFile, error) {
+	// Only substitute the file if it's not equal
+	isEqual, err := filediff.IsSameFile(in.FileName, in.EncodedContent)
 	if err != nil {
-		return &pb.FileDiff{}, err
+		return &pb.ProcessedFile{}, err
 	}
 
-	isEqual, err := filediff.IsSameFile(in.FileName, decodedContents)
+	if !isEqual {
+		if err := filediff.ReplaceFile(in.FileName, in.EncodedContent); err != nil {
+			return &pb.ProcessedFile{
+				Processed: false,
+				Error:     err.Error(),
+			}, nil
+		}
+
+		return &pb.ProcessedFile{
+			Processed: true,
+			FileName:  in.FileName,
+		}, nil
+	}
+
+	return &pb.ProcessedFile{
+		Processed: false,
+	}, nil
+}
+
+func (s *fileServer) CompareFile(ctx context.Context, in *pb.File) (*pb.FileDiff, error) {
+	isEqual, err := filediff.IsSameFile(in.FileName, in.EncodedContent)
 	if err != nil {
 		return &pb.FileDiff{}, err
 	}
